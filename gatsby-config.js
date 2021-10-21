ce9574c9ff3964bcf2fc4a9c520643e251d1f159
Esteban Benjumea <benjumeac12@gmail.com>
2021-10-21 13:17:37 -0500

    Add dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,23 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "My Briefcase",
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `My Briefcase`,
+        start_url: `/`,
+        icon: `./src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
+  ],
 };
